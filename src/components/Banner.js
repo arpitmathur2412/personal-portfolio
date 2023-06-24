0a908e577dd46688b894521d3bdcda4c74060fa2
Arpit Mathur <arpitmathur23@gmail.com>
2023-06-25 02:47:28 +0530

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
+  const [index,setIndex] = useState(1);
   const toRotate = [ "Web Developer", "Data Science explorer"];
   const period = 2000;
 
